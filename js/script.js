76bd0527cb79db45e8483555286074c8e7b4f9ad
Lykanos94 <luca77.94@libero.it>
2024-11-19 17:37:31 +0100

    base: exercise completed

--- js/script.js
+++ js/script.js
@@ -5,4 +5,32 @@ function checkparola(utenteparola) {
     } else {
         console.log("La parola non è palindroma");
     }  
-} checkparola(parola);                                  
+} checkparola(parola);
+
+
+
+// pari e dispari
+
+let sceltautente = prompt ("Chi vuoi che vinca pari o dispari?");
+let numero = prompt ("inserisci numero");
+let cpu = Math.floor((Math.random() * 5) + 1);
+function checknum (numero , cpu) {
+    const somma = numero + cpu;
+    console.log("questo è il numero dell'utente" + numero);
+    console.log("questo è il numero della cpu" + cpu);
+    if (sceltautente === "pari") {
+        if (somma % 2 === 0) {
+            console.log("Utente vince");
+        } else {
+            console.log("cpu vince");
+        }
+        
+    } else if (sceltautente === "dispari") {
+        if (somma % 2 !== 0) {
+            console.log("utente vince");
+        } else {
+            console.log("cpu vince");
+        }
+    }
+        
+} checknum(numero, cpu);
